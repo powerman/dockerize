4b54fe1f95a53200e12f83d1334e12a0202d234b
Calin Don <calin.don@gmail.com>
2018-08-24 12:49:55 +0300

    Add support for sprig function library

--- template.go
+++ template.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/url"
@@ -12,6 +11,7 @@ import (
 	"syscall"
 	"text/template"
 
+	"github.com/Masterminds/sprig"
 	"github.com/jwilder/gojq"
 )
 
@@ -26,49 +26,12 @@ func exists(path string) (bool, error) {
 	return false, err
 }
 
-func contains(item map[string]string, key string) bool {
-	if _, ok := item[key]; ok {
-		return true
-	}
-	return false
-}
-
-func defaultValue(args ...interface{}) (string, error) {
-	if len(args) == 0 {
-		return "", fmt.Errorf("default called with no values!")
-	}
-
-	if len(args) > 0 {
-		if args[0] != nil {
-			return args[0].(string), nil
-		}
-	}
-
-	if len(args) > 1 {
-		if args[1] == nil {
-			return "", fmt.Errorf("default called with nil default value!")
-		}
-
-		if _, ok := args[1].(string); !ok {
-			return "", fmt.Errorf("default is not a string value. hint: surround it w/ double quotes.")
-		}
-
-		return args[1].(string), nil
-	}
-
-	return "", fmt.Errorf("default called with no default value")
-}
-
 func parseUrl(rawurl string) *url.URL {
 	u, err := url.Parse(rawurl)
 	if err != nil {
 		log.Fatalf("unable to parse url %s: %s", rawurl, err)
 	}
 	return u
-}
-
-func add(arg1, arg2 int) int {
-	return arg1 + arg2
 }
 
 func isTrue(s string) bool {
@@ -91,45 +54,12 @@ func jsonQuery(jsonObj string, query string) (interface{}, error) {
 	return res, nil
 }
 
-func loop(args ...int) (<-chan int, error) {
-	var start, stop, step int
-	switch len(args) {
-	case 1:
-		start, stop, step = 0, args[0], 1
-	case 2:
-		start, stop, step = args[0], args[1], 1
-	case 3:
-		start, stop, step = args[0], args[1], args[2]
-	default:
-		return nil, fmt.Errorf("wrong number of arguments, expected 1-3"+
-			", but got %d", len(args))
-	}
-
-	c := make(chan int)
-	go func() {
-		for i := start; i < stop; i += step {
-			c <- i
-		}
-		close(c)
-	}()
-	return c, nil
-}
-
 func generateFile(templatePath, destPath string) bool {
-	tmpl := template.New(filepath.Base(templatePath)).Funcs(template.FuncMap{
-		"contains":  contains,
+	tmpl := template.New(filepath.Base(templatePath)).Funcs(sprig.TxtFuncMap()).Funcs(template.FuncMap{
 		"exists":    exists,
-		"split":     strings.Split,
-		"replace":   strings.Replace,
-		"default":   defaultValue,
 		"parseUrl":  parseUrl,
-		"atoi":      strconv.Atoi,
-		"add":       add,
 		"isTrue":    isTrue,
-		"lower":     strings.ToLower,
-		"upper":     strings.ToUpper,
 		"jsonQuery": jsonQuery,
-		"loop":      loop,
 	})
 
 	if len(delims) > 0 {
